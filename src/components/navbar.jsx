318dcfbe143db89fce383815f4fa7602accc7416
unknown <divyaneela75@gmail.com>
2024-07-10 16:50:33 +0530

    Initial commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
@@ -6,26 +6,39 @@ import logo from '../assets/ck_logo.png';
 
 export const CustomNavbar = () => {
   const location = useLocation();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClick = () => {
+    setExpanded(false);
+  };
 
   return (
-    <Navbar expand="lg" className="navbar-custom sticky-top pe-2 ps-2" style={{ boxSizing: 'border-box', borderBottom: '2px solid #e8eaf1' }}>
+    <Navbar
+      expand="lg"
+      className="navbar-custom sticky-top pe-2 ps-2"
+      style={{ boxSizing: 'border-box', borderBottom: '2px solid #e8eaf1' }}
+      expanded={expanded}
+      onToggle={() => setExpanded(!expanded)}
+    >
       <Navbar.Brand href="/">
         <img src={logo} alt="Logo" className="logo" />
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-        <Nav id='nav-link'>
-          {/* <Nav.Link
+        <Nav id="nav-link">
+          <Nav.Link
             as={Link}
             to="/"
             className={`nav-link-custom nav-underline ${location.pathname === '/' ? 'active' : ''}`}
+            onClick={handleNavClick}
           >
             Home
-          </Nav.Link> */}
+          </Nav.Link>
           <Nav.Link
             as={Link}
             to="/about"
             className={`nav-link-custom nav-underline ${location.pathname === '/about' ? 'active' : ''}`}
+            onClick={handleNavClick}
           >
             About
           </Nav.Link>
@@ -33,6 +46,7 @@ export const CustomNavbar = () => {
             as={Link}
             to="/service"
             className={`nav-link-custom nav-underline ${location.pathname === '/service' ? 'active' : ''}`}
+            onClick={handleNavClick}
           >
             Services
           </Nav.Link>
@@ -40,6 +54,7 @@ export const CustomNavbar = () => {
             as={Link}
             to="/news"
             className={`nav-link-custom nav-underline ${location.pathname === '/news' ? 'active' : ''}`}
+            onClick={handleNavClick}
           >
             News
           </Nav.Link>
@@ -47,15 +62,20 @@ export const CustomNavbar = () => {
             as={Link}
             to="/contact"
             className={`nav-link-custom nav-underline ${location.pathname === '/contact' ? 'active' : ''}`}
+            onClick={handleNavClick}
           >
             Contact
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
 
-      <div className='d-flex gap-2' id='nav-button'>
-        <button className='hiring-btn' style={{ width: '120px', padding: '.5rem', background: '#f6f6f4', color: 'black', border: 'none' }}>We're hiring</button>
-        <button style={{ width: '120px', padding: '.5rem', background: '#f85c1d', color: 'white', border: 'none' }}>Get Started</button>
+      <div className="d-flex gap-2" id="nav-button">
+        <button className="hiring-btn" style={{ width: '120px', padding: '.5rem', background: '#f6f6f4', color: 'black', border: 'none' }}>
+          We're hiring
+        </button>
+        <button style={{ width: '120px', padding: '.5rem', background: '#f85c1d', color: 'white', border: 'none' }}>
+          Get Started
+        </button>
       </div>
     </Navbar>
   );
