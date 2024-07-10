58b615002685546cee8a746c3acc2e4b5ff198be
unknown <divyaneela75@gmail.com>
2024-07-10 15:57:33 +0530

    Initial commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
-import logo from '../assets/ck_logo.jpeg';
+import logo from '../assets/ck_logo.png';
 
 export const CustomNavbar = () => {
   const location = useLocation();
 
   return (
-    <Navbar expand="lg" className="navbar-custom sticky-top" style={{ boxSizing: 'border-box', borderBottom: '2px solid #e8eaf1' }}>
+    <Navbar expand="lg" className="navbar-custom sticky-top pe-2 ps-2" style={{ boxSizing: 'border-box', borderBottom: '2px solid #e8eaf1' }}>
       <Navbar.Brand href="/">
         <img src={logo} alt="Logo" className="logo" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
         <Nav id='nav-link'>
-          <Nav.Link
+          {/* <Nav.Link
             as={Link}
             to="/"
             className={`nav-link-custom nav-underline ${location.pathname === '/' ? 'active' : ''}`}
           >
             Home
-          </Nav.Link>
+          </Nav.Link> */}
           <Nav.Link
             as={Link}
             to="/about"
@@ -52,6 +52,11 @@ export const CustomNavbar = () => {
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
+
+      <div className='d-flex gap-2' id='nav-button'>
+        <button className='hiring-btn' style={{ width: '120px', padding: '.5rem', background: '#f6f6f4', color: 'black', border: 'none' }}>We're hiring</button>
+        <button style={{ width: '120px', padding: '.5rem', background: '#f85c1d', color: 'white', border: 'none' }}>Get Started</button>
+      </div>
     </Navbar>
   );
 };
